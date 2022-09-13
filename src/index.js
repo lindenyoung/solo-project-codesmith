ae20669df6c25162ffb56b78957ce6469965a767
Linden Young <imlindenyoung@gmail.com>
2022-09-13 17:56:06 -0600

    connected styles to index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { render } from 'react-dom';
 import App from './components/App';
 
 // import styles
+import styles from './styles/app.scss';
 
 render(<App />, document.getElementById('root'));
